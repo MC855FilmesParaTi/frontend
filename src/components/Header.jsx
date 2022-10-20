ea026d980052e152e608b8a40f73ec31aeac7746
Ian Loron de Almeida <i198933@dac.unicamp.br>
2022-10-20 09:10:07 -0300

    home page skeleton ready

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import logo from "../assets/Logo.png";
+import SearchBox from "./SearchBox";
+
+const Header = () => {
+  return (
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <spam>Menu</spam>
+      <SearchBox />
+      <a
+        className="App-link"
+        href="/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        sign in
+      </a>
+    </div>
+  );
+};
+
+export default Header;
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Header from "../../components/Header";
 import styles from "./HomePage.module.scss";
+import MovieList from "../../components/MovieList";
+import MovieListHeading from "../../components/MovieListHeading";
 
 const HomePage = () => {
+  const movie = {
+    Poster: "https://news.artnet.com/app/news-upload/2015/04/lf-256x256.jpg",
+    Title: "La Dolce Vita",
+    Duration: "2h02min",
+  };
+
+  const movies = [movie, movie, movie];
+
   return (
     <div id="grad1" className={styles.App}>
       <Header />
+      <MovieListHeading heading="A Film List" />
+      <MovieList movies={movies} />
     </div>
   );
 };
